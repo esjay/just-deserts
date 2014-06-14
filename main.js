55dfd6dc61ae140834004157c963fac20b551e09
Ryan Plessner <ryan.plessner@cantinaconsulting.com>
2014-06-14 12:51:52 -0400

    removed requirejs and just created a global var

---
+++ main.js
@@ -0,0 +1,14 @@
+Game = {
+
+  start: function() {
+    Crafty.init(300, 300);
+
+    Crafty.background('black');
+
+    var box = Crafty.e('2D, Canvas, Color')
+      .attr({
+        x: 10, y: 10, w: 100, h: 100
+      })
+      .color('green');
+  }
+};
