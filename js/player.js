054fe525ad3ab30cc60bd58cc11b58b51691b919
tomyurek <tomyurek@gmail.com>
2014-06-14 17:57:51 -0500

    Motion system is now workable
    but not perfect

--- js/player.js
+++ js/player.js
@@ -6,7 +6,6 @@ define(['crafty'], function(Crafty) {
 	  var DEFAULT_SPEED = 5;
 	  var LEVEL_WIDTH = 900;
 	  var jumpspeed = 0;
-	  var ylast = -1; //The program compares the character's current y position to the y position last cycle to determine if he is on a platform.
 	  var canjump = false;
 	  var jumptapering = false; //Makes downward fall once up arrow is released smoother
 	  var jumptimer = 0; //After a while, whether or not you're holding down the up key won't matter
@@ -17,8 +16,17 @@ define(['crafty'], function(Crafty) {
 		  .bind('EnterFrame', function() //EnterFrame event is called once per cycle
 		  {
 			//Moves left and right
-			if(this.isDown('LEFT_ARROW')){this.x = this.x - DEFAULT_SPEED};
-			if(this.isDown('RIGHT_ARROW')){this.x = this.x + DEFAULT_SPEED};
+			if(this.isDown('LEFT_ARROW'))
+			{
+				this.x -= DEFAULT_SPEED;
+				if(this.hit('PGrav')){this.x += DEFAULT_SPEED};
+			}
+			if(this.isDown('RIGHT_ARROW'))
+			{
+				this.x += DEFAULT_SPEED;
+				if(this.hit('PGrav')){this.x -= DEFAULT_SPEED};
+			}
+			//
 			//Keeps character from going off course
 			if (this.x < 0){this.x = 0};
 			if (this.x > LEVEL_WIDTH + this.w){this.x = LEVEL_WIDTH + this.w};
@@ -34,27 +42,25 @@ define(['crafty'], function(Crafty) {
 					yaccel = 0;
 				}
 			}
-			else
-			{
-				//The purpose of this block is to determine which side of whatever wall we're on so we don't jump through walls
-				center = this.x;
-				for(var i = 1; true; i++)
-				{
-					this.x = center - i;
-					if(!this.hit('PGrav')){break;}
-					this.x = center + i;
-					if(!this.hit('PGrav')){break;}
-				}
-			}
 			//Jumping
-			canjump = (jumpspeed == 0 && ylast == this.y);
-			ylast = this.y;
+			canjump = yaccel == 0 ;
 			if (this.isDown('UP_ARROW') && canjump)
 			{
 				jumpspeed = 10;
 				canjump = false;
 			}
 			this.y -= jumpspeed;
+			if(this.hit('PGrav'))
+			{
+				while(this.hit('PGrav'))
+				{
+					this.y += 1;
+					jumpspeed = 0;
+					jumptapering = false;
+					yaccel = 0;
+					jumptimer = 0;
+				}
+			}
 			if(jumptapering && jumpspeed > 0)
 			{
 				jumpspeed -= 1;
