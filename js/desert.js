1362390d11c32f5fd9a99c73a1ae8a339ab3e05a
Ryan Plessner <ryan.plessner@cantinaconsulting.com>
2014-06-15 15:35:22 -0400

    made the ending text bigger

--- js/desert.js
+++ js/desert.js
@@ -8,8 +8,9 @@ define(['crafty', '../world', '../levels/level1', '../hud'],
    Crafty.defineScene('GameOver', function(score) {
      Crafty.viewport.scroll('x', 0);
      Crafty.e('2D, Canvas, DOM, Color, Text')
-           .attr({ x: 500, y: 400, w: 100, h: 20})
-           .text("Game Over: " + score);
+           .attr({ x: 400, y: 400, w: 100, h: 20})
+           .textFont({family: "Georgia", size: 18})
+           .text("You Survived with " + Math.round(score) + " health Left");
    });
 
   Crafty.enterScene('Game');
