6fc00e5b242af433381a852ff9d37f81179a97f9
Ryan Plessner <ryan.plessner@cantinaconsulting.com>
2014-06-14 22:29:14 -0400

    the hud now scrolls

--- js/hud.js
+++ js/hud.js
@@ -1,21 +1,30 @@
 define(['crafty'], function(Crafty) {
   return {
     createText: function() {
+      var offset = this._getViewOffsets();
       this.thirstEl = Crafty.e("2D, DOM, Text")
-                      .attr({ x: 100, y: 100 })
+                      .attr({ x: 100 + offset.x, y: 100 + offset.y })
                       .textFont({family: "Georgia", size: 18})
                       .text("Thirst: ");
       this.healthEl = Crafty.e("2D, DOM, Text")
-                .attr({ x: 200, y: 100 })
+                .attr({ x: 200 + offset.x, y: 100 + offset.y })
                 .textFont({family: "Georgia", size: 18})
                 .text("Health: ");
 
     },
     pushThirst: function(thirstVal) {
+      var offset = this._getViewOffsets();
+      this.thirstEl.attr({ x: 100 + offset.x, y: 100 + offset.y });
       this.thirstEl.text("Thirst: " + thirstVal);
+
     },
     pushHealth: function(healthVal) {
+      var offset = this._getViewOffsets();
+      this.healthEl.attr({ x: 200 + offset.x, y: 100 + offset.y });
       this.healthEl.text("Health: " + healthVal);
+    },
+    _getViewOffsets: function() {
+      return { x: -(Crafty.viewport.x), y: -(Crafty.viewport.y) };
     }
   };
 });
