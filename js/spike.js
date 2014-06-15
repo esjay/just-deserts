7ed95bb7bbded4d3c9efa31f9d0875f6ac5a97e1
Ryan Plessner <ryan.plessner@cantinaconsulting.com>
2014-06-15 14:47:30 -0400

    added the new spike image

--- js/spike.js
+++ js/spike.js
@@ -1,5 +1,5 @@
 define(['crafty'], function(Crafty){
-  Crafty.sprite("assets/img/spike.png", {spike_gfx:[0,0,40,40]});
+  Crafty.sprite("assets/img/rail_spike_large.png", {spike_gfx:[0,0,40,80]});
   Crafty.c('Spike', {
     init: function() {
       this.requires('2D, Canvas, spike_gfx');
