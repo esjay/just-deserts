26606a1a78eb88933932369299e7df12d220f524
Ryan Plessner <ryan.plessner@cantinaconsulting.com>
2014-06-15 09:21:20 -0400

    now moving the HUD correctly

--- js/hud.js
+++ js/hud.js
@@ -1,30 +1,28 @@
 define(['crafty'], function(Crafty) {
   return {
     createText: function() {
-      var offset = this._getViewOffsets();
       this.thirstEl = Crafty.e("2D, DOM, Text")
-                      .attr({ x: 100 + offset.x, y: 100 + offset.y })
+                      .attr({ x: 100 -(Crafty.viewport.x), y: 100 })
                       .textFont({family: "Georgia", size: 18})
-                      .text("Thirst: ");
+                      .text("Thirst: ")
+                      .bind("EnterFrame", function() {
+                        this.attr({ x: 100 -(Crafty.viewport.x), y: 100 });
+                      });
       this.healthEl = Crafty.e("2D, DOM, Text")
-                .attr({ x: 200 + offset.x, y: 100 + offset.y })
+                .attr({ x: 200 -(Crafty.viewport.x), y: 100 })
                 .textFont({family: "Georgia", size: 18})
-                .text("Health: ");
+                .text("Health: ")
+                .bind("EnterFrame", function() {
+                  this.attr({ x: 200 -(Crafty.viewport.x), y: 100 });
+                });
 
     },
     pushThirst: function(thirstVal) {
-      var offset = this._getViewOffsets();
-      this.thirstEl.attr({ x: 100 + offset.x, y: 100 + offset.y });
       this.thirstEl.text("Thirst: " + thirstVal);
 
     },
     pushHealth: function(healthVal) {
-      var offset = this._getViewOffsets();
-      this.healthEl.attr({ x: 200 + offset.x, y: 100 + offset.y });
       this.healthEl.text("Health: " + healthVal);
-    },
-    _getViewOffsets: function() {
-      return { x: -(Crafty.viewport.x), y: -(Crafty.viewport.y) };
     }
   };
 });
