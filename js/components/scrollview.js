bfcbe0b5076c7bba285ffde4a65da286cd9be326
Ryan Plessner <ryan.plessner@cantinaconsulting.com>
2014-06-15 10:01:51 -0400

    fixed the bug where the viewport doesn't reset correctly when you hit a spike

--- js/components/scrollview.js
+++ js/components/scrollview.js
@@ -3,23 +3,28 @@ define(['crafty'], function(Crafty) {
   Crafty.c('ScrollView', {
     viewport: Crafty.viewport,
     oldX: 0,
+    resetView: false,
 
     init: function() {
       this.requires('2D, Keyboard')
           .bind('EnterFrame', function() {
-            if (this.isDown('LEFT_ARROW') && this.viewport.x <= 0 && this.oldX !== this.x) {
-              this.viewport.scroll('x', this.viewport.x + Math.abs(this.x - this.oldX));
-            } else if(this.isDown('RIGHT_ARROW')) {
-              if (400 <= this.x && this.worldData.width > (1024 - this.viewport.x)) {
-                this.viewport.scroll('x', this.viewport.x - Math.abs(this.x - this.oldX));
+            if (!this.resetView) {
+              if (this.isDown('LEFT_ARROW') && this.viewport.x <= 0 && this.oldX !== this.x) {
+                this.viewport.scroll('x', this.viewport.x + Math.abs(this.x - this.oldX));
+              } else if(this.isDown('RIGHT_ARROW')) {
+                if (400 <= this.x && this.worldData.width > (1024 - this.viewport.x)) {
+                  this.viewport.scroll('x', this.viewport.x - Math.abs(this.x - this.oldX));
+                }
               }
             }
+            this.resetView = false;
             this.oldX = this.x;
           });
     },
 
     resetScroll: function() {
       this.viewport.scroll('x', 0);
+      this.resetView = true;
     }
   });
 
